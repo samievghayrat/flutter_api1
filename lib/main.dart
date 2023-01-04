820bcd3c7b1ebfb7f11e064a37b7e1d08861e03a
ghayratsamiev <Ghayrat Samiev>
2023-01-05 02:04:29 +0900

    exchanging data between the screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,107 @@
 import 'package:flutter/material.dart';
-import 'classes_and_functions.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.teal,
+      ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Sample Shopping App"),
-        ),
-        body: ShoppingList(
-          products: [
-            Product(name: 'Eggs'),
-            Product(name: 'Flour'),
-            Product(name: 'Chocolate chips'),],
-        ),
+      title: 'Navigation Basics',
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController frontController = TextEditingController();
+  TextEditingController backController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Route'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8),
+            child:  Center(
+              child: TextField(
+                controller: frontController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Front",
+                    labelStyle: TextStyle(fontSize: 18)),
+              ),
+            ),
+          ),
+           Center(
+            child: TextField(
+              controller: backController,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Back",
+                  labelStyle: TextStyle(fontSize: 18)),
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: ElevatedButton(
+                child: Text('Second Route'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SecondRoute(front: frontController.text, back: backController.text,)),
+                  );
+                },
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
 }
 
-class Shopping extends StatelessWidget {
-  Shopping({required this.product,
-    required this.inCard,
-    required this.onCardChanged})
-      : super(key: ObjectKey(product));
-  final Product product;
-  final bool inCard;
-  final CardChangedCallBack onCardChanged;
+class SecondRoute extends StatefulWidget {
+  late final  String front;
+  late final String back;
+  SecondRoute({super.key, required this.back,required this.front});
+  @override
+  State<SecondRoute> createState() => _SecondRouteState();
+}
 
-  Color _getColor(BuildContext context) {
-    return inCard ? Colors.black54 : Theme
-        .of(context)
-        .primaryColor;
-  }
-
-  TextStyle? _getTextStyle(BuildContext context) {
-    if (!inCard) return null;
-    return const TextStyle(
-        color: Colors.black54, decoration: TextDecoration.lineThrough);
-  }
-
+class _SecondRouteState extends State<SecondRoute> {
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {
-        onCardChanged(product, inCard);
-      },
-      leading: CircleAvatar(
-        backgroundColor: _getColor(context),
-        child: Text(product.name[0]),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
       ),
-      title: Text(
-        product.name,
-        style: _getTextStyle(context),
+      body: Column(
+        children: [
+          Text(widget.front),
+          Text(widget.back),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Go back!'),
+            ),
+          ),
+        ],
       ),
     );
-  }
-}
-
-class ShoppingList extends StatefulWidget {
-  // ignore: use_key_in_widget_constructors
-  const ShoppingList({required this.products, super.key});
-  final List<Product> products;
-  @override
-  State<ShoppingList> createState() => _ShoppingListState();
-}
-class _ShoppingListState extends State<ShoppingList> {
-  final _shoppingCart = <Product>{};
-  void _handleCardChanges(Product product, bool inCart) {
-    setState(() {
-      if (!inCart) {
-        _shoppingCart.add(product);
-      } else {
-        _shoppingCart.remove(product);
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: ListView(padding: const EdgeInsets.symmetric(vertical: 8.0),
-      children: widget.products.map((product) {
-        return Shopping(product: product,
-          inCard: _shoppingCart.contains(product),
-          onCardChanged: _handleCardChanges);
-      }).toList(),),);
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flutter_api/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( Shopping(product: Product(name: "chips"), inCard: true, onCardChanged: (product, inCard){},));
+    await tester.pumpWidget( MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
