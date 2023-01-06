41948feff2555f50cb13ea7b9a7e097c42458313
ghayratsamiev <Ghayrat Samiev>
2023-01-06 15:35:00 +0900

    2 screen data exchange

--- lib/main.dart
+++ lib/main.dart
@@ -32,51 +32,60 @@ class _MyAppState extends State<MyApp> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child:  Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: Center(
+                child: TextField(
+                  controller: frontController,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Front",
+                      labelStyle: TextStyle(fontSize: 18)),
+                ),
+              ),
+            ),
+            Center(
               child: TextField(
-                controller: frontController,
+                controller: backController,
                 decoration: const InputDecoration(
                     border: OutlineInputBorder(),
-                    labelText: "Front",
+                    labelText: "Back",
                     labelStyle: TextStyle(fontSize: 18)),
               ),
             ),
-          ),
-           Center(
-            child: TextField(
-              controller: backController,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Back",
-                  labelStyle: TextStyle(fontSize: 18)),
-            ),
-          ),
-          Expanded(
-            child: Center(
-              child: ElevatedButton(
-                child: Text('Second Route'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SecondRoute(front: frontController.text, back: backController.text,)),
-                  );
-                },
+            Expanded(
+              child: Center(
+                child: ElevatedButton(
+                  child: const Text('Second Route'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SecondRoute(
+                          front: frontController.text,
+                          back: backController.text,
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-        ]),
+          ],
+        ),
       ),
     );
   }
 }
 
 class SecondRoute extends StatefulWidget {
-  late final  String front;
+  late final String front;
   late final String back;
-  SecondRoute({super.key, required this.back,required this.front});
+
+  SecondRoute({super.key, required this.back, required this.front});
+
   @override
   State<SecondRoute> createState() => _SecondRouteState();
 }
@@ -90,12 +99,30 @@ class _SecondRouteState extends State<SecondRoute> {
       ),
       body: Column(
         children: [
-          Text(widget.front),
-          Text(widget.back),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(40)),
+              height: 160,
+              width: 200,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(widget.front),
+                  Text(widget.back),
+                ],
+              ),
+            ),
+          ),
           Center(
             child: ElevatedButton(
               onPressed: () {
                 Navigator.pop(context);
+                Navigator.pop(context);
+                widget.front = '';
+                widget.back = '';
               },
               child: const Text('Go back!'),
             ),
